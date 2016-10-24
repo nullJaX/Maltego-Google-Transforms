3acd4376a7f3f7d4109f2d2cf71de368ad58b85c
nullJaX <195643@edu.p.lodz.pl>
2016-10-24 19:21:12 +0200

    Improved transform and entity

--- Transforms/toGoogleUser.py
+++ Transforms/toGoogleUser.py
@@ -6,27 +6,30 @@ import httplib2
 import apiclient.discovery
 from MaltegoTransform import *
 
+#initialize maltego transform and retrieve input entity value
 me = MaltegoTransform()
 me.parseArguments(sys.argv)
 Q = me.getValue()
-#debug
-#Q = sys.argv[1]
 API_KEY = "AIzaSyCQUjsJBAKSLNqM187Yro9z8lFO8KUXyVE"
 
+#initialize G+
 service = apiclient.discovery.build('plus', 'v1', http=httplib2.Http(), developerKey=API_KEY)
+
+#get query result
 people_feed = service.people().search(query=Q).execute()
-enities = []
+
+
 for person in people_feed['items']:
 	preImageURL = person['image']
 	imageURL = preImageURL['url']
-	ent = me.addEntity("nullJaX.Affiliation-Google-Plus",person['displayName'])
+	ent = me.addEntity("nullJaX.Affiliation-Google-Plus",person['id'])
 	ent.setType("nullJaX.Affiliation-Google-Plus")
-	ent.setValue(person['displayName'])
-	ent.addAdditionalFields("UID","UID",True,person['id'])
+	ent.setValue(person['id'])
+	ent.addAdditionalFields("Name","Name",True,person['displayName'])
 	ent.addAdditionalFields("Profile URL","Profile URL", True, person['url'])
-	ent.addAdditionalFields("Avatar URL","Avatar URL",True, imageURL)
 	ent.setIconURL(imageURL)
 	me.heartbeat()
-	ent.returnEntity()
 	#print(person['displayName']+" : "+person['id']+" : "+imageURL)
 me.returnOutput()
+
+#TODO: add try/except clause to provide better UX :)
