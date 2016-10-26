ee14b2866e6c7321ebdf7681c649960fe6238063
nullJaX <195643@edu.p.lodz.pl>
2016-10-26 15:37:55 +0200

    Second transform almost finished.
    Improved Entity fields.

--- Transforms/FromGoogleUserToGoogleUserDetails.py
+++ Transforms/FromGoogleUserToGoogleUserDetails.py
@@ -21,15 +21,55 @@ try:
 	person = service.people().get(userId=Q).execute()
 	
 	#basic info about entity
-	m = me.addEntity('nullJaX.GooglePlusPeopleResource',person['nickname'])
-	m.setType('nullJaX.GooglePlusPeopleResource')
+	m = me.addEntity("nullJaX.GooglePlusPeopleResource",person['nickname'])
+	m.setType("nullJaX.GooglePlusPeopleResource")
 	m.setValue(person['nickname'])
 	m.setDisplayInformation(person['nickname'])
 	m.setIconURL(person['image']['url'])
 	
 	#add all fields - TODO
-	m.addAdditionalFields()
-
+	m.addAdditionalFields("UID","UID",True,person['id'])
+	m.addAdditionalFields("displayName","Display Name",True,person['displayName'])
+	m.addAdditionalFields("givenName","First Name",True,person['name']['givenName'])
+	m.addAdditionalFields("middleName","Middle Name",True,person['name']['middleName'])
+	m.addAdditionalFields("familyName","Surname",True,person['name']['familyName'])
+	m.addAdditionalFields("honorificPrefix","Honorific Prefix",True,person['name']['honorificPrefix'])
+	m.addAdditionalFields("honorificSuffix","Honorific Suffix",True,person['name']['honorificSuffix'])
+	m.addAdditionalFields("birthday","Date of birth",True,person['birthday'])
+	m.addAdditionalFields("gender","Gender",True,person['gender'])
+	m.addAdditionalFields("url","Profile URL",True,person['url'])
+	m.addAdditionalFields("aboutMe","Biography",True,person['aboutMe'])
+	relationshipStatus = person['relationshipStatus'].replace("_"," ")
+	m.addAdditionalFields("relationshipStatus","Relationship Status",True,relationshipStatus)
+	age_range = person['ageRange']['min']+" "+person['ageRange']['max']
+	m.addAdditionalFields("ageRange","Age Range",True,age_range)
+	emails = ""
+	for email in person['emails']:
+		emails += "("+email['type']+") "+email['value']+"; "
+	
+	m.addAdditionalFields("emails","Emails",True,emails)
+	m.addAdditionalFields("occupation","Occupation",True,person['occupation'])
+	m.addAdditionalFields("skills","Skills",True,person['skills'])
+	urls = ""
+	for url in person['urls']:
+		urls += "("+url['label']+":"+url['type']+") "+url['value']+"; "
+		
+	m.addAdditionalFields("urls","URLs related",True,urls)
+	
+	#add note with organizations details - TODO
+	organizations = ""
+	for organization in person['organizations']:
+		organizations += organization['name']
+	m.addAdditionalFields("organizations","Organizations",True,organizations)
+	
+	places = ""
+	for place in person['placesLived']:
+		if(place['primary']):
+			places += "(primary residence) "
+		places += place['value']+"; "
+	m.addAdditionalFields("placesLived","Places",True,places)
+	
+	me.heartbeat()
 	me.returnOutput()
 except:
 	me.addException("Something went wrong :(")
